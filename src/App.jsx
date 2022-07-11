2b5e1d0f0dbb8017949f5cec7973099494b18c8d
TaneriX07 <tanerivince@gmail.com>
2022-07-11 21:34:35 +0800

    Finish reducer implementation and integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,48 @@
-import { useSelector } from "react-redux";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  const [n, setN] = useState(2);
+
+  const handleInc = () => {
+    dispatch({ type: "counter/inc" });
+  };
+
+  const handleDec = () => {
+    dispatch({ type: "counter/dec" });
+  };
+
+  const handleIncByN = () => {
+    dispatch({ type: "counter/incByN", payload: n });
+  };
+
+  const handleDecByN = () => {
+    dispatch({ type: "counter/decByN", payload: n });
+  };
 
   return (
     <div className="App">
       <img src={logo} className="App-logo" alt="logo" />
-      <p>Counter App with React and Redux</p>
-      <p>
-        <button type="button">count is: {counter}</button>
-      </p>
+      <h2>Counter App with React and Redux</h2>
+      <p>The current count is: {counter}</p>
+      <div>
+        <button onClick={handleInc}>Inc</button>
+        <button onClick={handleDec}>Dec</button>
+      </div>
+      <input
+        type="number"
+        placeholder="n"
+        value={n}
+        onChange={(e) => setN(parseInt(e.target.value))}
+      />
+      <div>
+        <button onClick={handleIncByN}>Inc by n</button>
+        <button onClick={handleDecByN}>Dec by n</button>
+      </div>
     </div>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,18 @@ const initialCounterState = {
 };
 
 const counterReducer = (state = initialCounterState, action) => {
-  return state;
+  switch (action.type) {
+    case "counter/inc":
+      return { ...state, counter: state.counter + 1 };
+    case "counter/dec":
+      return { ...state, counter: state.counter - 1 };
+    case "counter/incByN":
+      return { ...state, counter: state.counter + action.payload };
+    case "counter/decByN":
+      return { ...state, counter: state.counter - action.payload };
+    default:
+      return state;
+  }
 };
 
 const store = createStore(counterReducer);
